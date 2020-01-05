a81eeea0770d3eec8fd83f6dd9261476e917fa58
Weikun Wu <wwu1997@berkeley.edu>
2020-01-04 22:43:44 -0800

    changed signup form name

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class CreateUserForm(UserCreationForm):
+class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=254, required=True)
