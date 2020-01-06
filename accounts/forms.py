9ad851b54bbdf38434f1ad6901b882dc41bd3dc0
Weikun Wu <wwu1997@berkeley.edu>
2020-01-05 22:59:08 -0800

    added more fields and a save method

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,30 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+import pdb
 
-
-class SignUpForm(UserCreationForm):
+class UserSignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=254, required=True)
+    alternate_email = forms.EmailField(max_length=254, required=False, help_text=
+                'Preferably at least one of your email addresses on record will be'+
+                 'a non-edu address (e.g., @gmail.com, @yahoo.com).')
+    birth_date = forms.DateField(label='Birth Date',
+        help_text='Required. Format: YYYY-MM-DD')
+    preferred_name = forms.CharField(max_length=30, required=False,
+                            help_text='What would you like us to call you?')
 
     class Meta:
             model = User
-            fields = ('username', 'first_name', 'last_name',\
-             'email', 'password1', 'password2', )
+            fields = ('username', 'first_name', 'last_name','password1', 'password2',
+                 'email','birth_date' ,'alternate_email', 'preferred_name',)
+
+    def save(self,commit=True):
+        user = super(SignUpForm,self).save(commit=False)
+        for key,value in self.cleaned_data.items():
+            pdb.set_trace()
+            user.__dict__[key] = value
+        if commit:
+            user.save()
+        return user
